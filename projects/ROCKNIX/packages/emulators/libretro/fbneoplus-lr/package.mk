8b026414bacf1108091627cf875644ec62cff368
Adri <epsilondevteam@gmail.com>
2026-06-21 09:29:35 +0200

    fix(fbneoplus-lr): use PKG_BUILD for absolute paths in pre_configure_target

--- projects/ROCKNIX/packages/emulators/libretro/fbneoplus-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/fbneoplus-lr/package.mk
@@ -11,9 +11,9 @@ PKG_LONGDESC="Port of Final Burn Neo Plus to Libretro."
 PKG_TOOLCHAIN="make"
 
 pre_configure_target() {
-  sed -i "s|LDFLAGS += -static-libgcc -static-libstdc++|LDFLAGS += -static-libgcc|" ./src/burner/libretro/Makefile
+  sed -i "s|LDFLAGS += -static-libgcc -static-libstdc++|LDFLAGS += -static-libgcc|" ${PKG_BUILD}/src/burner/libretro/Makefile
 
-  PKG_MAKE_OPTS_TARGET=" -C ./src/burner/libretro USE_CYCLONE=0 profile=performance"
+  PKG_MAKE_OPTS_TARGET=" -C ${PKG_BUILD}/src/burner/libretro USE_CYCLONE=0 profile=performance"
 
   if [[ "${TARGET_FPU}" =~ "neon" ]]; then
     PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
